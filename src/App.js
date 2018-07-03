0a49c5c3d9a19f78e11c2dd5d4d68424b472428a
Gareth <williams.gareth0@gmail.com>
2018-07-03 12:36:37 +0200

    added flat search functionality

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      allFlats: []
     };
   }
 
@@ -33,7 +34,9 @@ class App extends Component {
       .then((data) => {
         this.setState({
           flats: data,
-          selectedFlat: null
+          allFlats: data,
+          selectedFlat: null,
+          search: ""
         });
       });
   }
@@ -43,6 +46,13 @@ class App extends Component {
       selectedFlat: flat
     })
   };
+
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    })
+  }
 
   render() {
     let center = {
@@ -61,7 +71,9 @@ class App extends Component {
     return (
      <div className="app">
       <div className="main">
-        <div className="search"></div>
+        <div className="search">
+          <input type="text" placeholder="Search..." value={this.state.search} onChange={this.handleSearch} />
+        </div>
         <div className="flats">
           {this.state.flats.map((flat) => {
             return <Flat key={flat.name} flat={flat} selectFlat={this.selectFlat}/>
