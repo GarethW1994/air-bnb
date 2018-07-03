a547784d786917ec0cc6794c1d2305453c061fcd
Gareth <williams.gareth0@gmail.com>
2018-07-03 10:28:22 +0200

    create state, call json from gh and change state to render flats from gh

--- src/App.js
+++ src/App.js
@@ -16,13 +16,31 @@ const flat =  {
 const flats = [flat, flat, flat, flat];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      });
+  }
+
   render() {
     return (
      <div className="app">
       <div className="main">
         <div className="search"></div>
         <div className="flats">
-          {flats.map((flat) => {
+          {this.state.flats.map((flat) => {
             return <Flat flat={flat} />
           })}
         </div>
